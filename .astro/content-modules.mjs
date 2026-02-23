539b4548f18001bef73fb0eae48b3d9a6182ede6
Alex Bogdanovski <alex@erudika.com>
2026-02-23 16:56:33 +0200

    initial generation - done

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,9 @@
+
+export default new Map([
+["src/content/docs/documentation/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocumentation%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/documentation/deployment/cloud.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocumentation%2Fdeployment%2Fcloud.mdx&astroContentModuleFlag=true")],
+["src/content/docs/documentation/deployment/docker.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocumentation%2Fdeployment%2Fdocker.mdx&astroContentModuleFlag=true")],
+["src/content/docs/documentation/getting-started/configuration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocumentation%2Fgetting-started%2Fconfiguration.mdx&astroContentModuleFlag=true")],
+["src/content/docs/documentation/getting-started/installation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocumentation%2Fgetting-started%2Finstallation.mdx&astroContentModuleFlag=true")],
+["src/content/docs/documentation/scoold-api/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocumentation%2Fscoold-api%2Findex.mdx&astroContentModuleFlag=true")]]);
+		
---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  site: 'https://scoold.com',
+  integrations: [
+    // Register sitemap BEFORE Starlight to prevent Starlight adding its own version
+    sitemap(),
+    starlight({
+      title: 'Scoold Documentation',
+      logo: {
+        src: './public/favicon.svg',
+      },
+      customCss: ['./src/styles/starlight.css'],
+      social: {
+        github: 'https://github.com/Erudika/scoold',
+      },
+      sidebar: [
+        {
+          label: 'Getting Started',
+          autogenerate: { directory: 'documentation/getting-started' },
+        },
+        {
+          label: 'Deployment',
+          autogenerate: { directory: 'documentation/deployment' },
+        },
+        { label: 'API Reference', autogenerate: { directory: 'documentation/scoold-api' } },
+      ],
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: 'static',
+});
